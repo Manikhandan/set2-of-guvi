d6695980c6d3a22fc5b1503832ab3bd344e1b36f
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-01-21 04:19:17 +0000

    Create multi5.c

---
+++ multi5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int k=0,i,n;
+printf("enter a values of n");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+    k=i*5;
+printf("the  multiplied values are%d\n",k);
+}
+}
