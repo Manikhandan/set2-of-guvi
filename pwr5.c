c7c4953e05866d1fea579004edb655de33ea6c97
JAYARAMGITHUB <35127780+JAYARAMGITHUB@users.noreply.github.com>
2018-01-21 04:35:21 +0000

    Create pwr5.c

---
+++ pwr5.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int i,o,r;
+printf(" enter the number");
+scanf("%d",&i);
+printf("enter the value of power");
+scanf("%d",&o);
+r=pow(i,o);
+printf("%d",r);
+}
